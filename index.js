3067c1d9aa4b9207719e4111b7e7e51aa52f266e
William Giammona <wgiammona@gmail.com>
2024-01-07 18:42:06 +0200

    start on DOM

--- index.js
+++ index.js
@@ -5,12 +5,3 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove("menu--open");
 }
-
-const fn = (arr) => {
-  let newArr = arr.map((item) => {
-    return !!item == true ? true : false;
-  });
-  return newArr;
-};
-
-console.log(fn(["CocoMooja", 4, 0, 3, "", false, "apple", true]));
