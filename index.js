9e82e7c1d5319e94e391477ec0a9f012c5ff8e01
William Giammona <wgiammona@gmail.com>
2024-01-08 12:10:01 +0200

    sorted

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ function closeMenu() {
 }
 
 const adv = (arr) => {
-  return starStr;
+  let sortedArray = arr.sort((a, b) => {
+    return a - b;
+  });
+  return sortedArray;
 };
 
-console.log(adv(5));
+console.log(adv([5, 40, 0, -5]));
