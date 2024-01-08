9e2c0f399da8cd9725dd00070c310b1fe9ce7c60
William Giammona <wgiammona@gmail.com>
2024-01-08 12:27:49 +0200

    sorting objects

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ function closeMenu() {
 
 const adv = (arr) => {
   let sortedArray = arr.sort((a, b) => {
-    return a - b;
+    return a.price - b.price;
   });
   return sortedArray;
 };
 
-console.log(adv([5, 40, 0, -5]));
+console.log(
+  adv([
+    { id: 1, price: 50 },
+    { id: 2, price: 30 },
+    { id: 3, price: 60 },
+    { id: 4, price: 10 },
+  ])
+);
