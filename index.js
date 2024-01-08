6ee0cd28cc677865c39a7534b6f69320313c93da
William Giammona <wgiammona@gmail.com>
2024-01-08 12:36:43 +0200

    finished practice

--- index.js
+++ index.js
@@ -5,19 +5,3 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove("menu--open");
 }
-
-const adv = (arr) => {
-  let sortedArray = arr.sort((a, b) => {
-    return a.price - b.price;
-  });
-  return sortedArray;
-};
-
-console.log(
-  adv([
-    { id: 1, price: 50 },
-    { id: 2, price: 30 },
-    { id: 3, price: 60 },
-    { id: 4, price: 10 },
-  ])
-);
